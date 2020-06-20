4a8620045bba1dd5ee17b40564373ad6ab8e02e3
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-20 20:03:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,5 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-  i = nil
+  i = 
   ans = ""
   name_hash.collect do |tag, value|
     if i > value
